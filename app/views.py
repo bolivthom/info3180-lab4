5c8cd39a5a693b530f0bdfd3e2c62dd8b8403105
Brittany <bolivthom@gmail.com>
2019-01-29 22:53:44 +0000

    uploading added

--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from forms import UploadForm
+rootdir = os.getcwd()
+print rootdir
+
 
 
 ###
@@ -28,20 +32,21 @@ def about():
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
+    error = None
     if not session.get('logged_in'):
         abort(401)
-
-    # Instantiate your form class
-
+    form = UploadForm()
     # Validate file upload on submit
     if request.method == 'POST':
-        # Get file data and save to your uploads folder
-
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
-
-    return render_template('upload.html')
-
+        if form.validate() == False:
+            flash_errors(form)
+        else:
+            file = form.upload.data
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File Saved', 'success')
+            return redirect(url_for('home', error=error))
+    return render_template('upload.html', form = form, error=error)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
